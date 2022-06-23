332189ddb9d77ca969d83dbead5f61f3000c714a
me.45 <me.45@TeamCity>
2022-06-23 16:11:08 +0000

    TeamCity change in 'Open-source projects / Rust-Multiplatform / Base Project Template' project: runners of 'Build' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -47,6 +47,7 @@ object Build : BuildType({
             name = "Build (Debug)"
             type = "cargo"
             executionMode = BuildStep.ExecutionMode.RUN_ON_FAILURE
+            param("cargo-build-package", "platform_linux")
             param("cargo-toolchain", "stable")
             param("cargo-verbosity", "--verbose")
             param("cargo-bench-package", "platform_linux")
