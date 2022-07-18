17654966f37b01aeb5e0671da1c173713ba1ce5c
@Sakul6499 | Lukas Weber <me@sakul6499.de>
2022-07-18 18:40:03 +0200

    Added example usage of 'ui-tests' flag to platform macOS

--- platform/macos/src/tests/mod.rs
+++ platform/macos/src/tests/mod.rs
@@ -4,3 +4,10 @@ use crate::main;
 fn check_main_exists() {
     main();
 }
+
+// Example for UI tests:
+// #[test]
+// #[cfg(feature = "ui-tests")]
+// fn some_ui_test() {
+//     assert!(true);
+// }
