d6903e938a2e18ab6a979c77a5e0634de13a77dc
@Sakul6499 | Lukas Weber <me@sakul6499.de>
2022-07-18 20:19:43 +0200

    Added missing ui-tests example

--- shared/src/tests/mod.rs
+++ shared/src/tests/mod.rs
@@ -5,3 +5,10 @@ fn check_entrypoint_exists() {
     println!("This requires a Desktop Environment up and running! Tests will fail otherwise.");
     entrypoint();
 }
+
+// Example for UI tests:
+// #[test]
+// #[cfg(feature = "ui-tests")]
+// fn some_ui_test() {
+//     assert!(true);
+// }
