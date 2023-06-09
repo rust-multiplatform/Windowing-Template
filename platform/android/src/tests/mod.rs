8bf7afb20c4e41a5e3f87c8f6e8cfe8cbed2ea03
Lukas Weber <dev@sakul-flee.de>
2023-06-09 03:31:42 +0200

    Fixed android test

--- platform/android/src/tests/mod.rs
+++ platform/android/src/tests/mod.rs
@@ -1,8 +1,8 @@
-use crate::main;
+use crate::android_main;
 
 #[test]
 #[cfg(feature = "ui-tests")]
 fn check_main_exists() {
     println!("This requires a Desktop Environment up and running! Tests will fail otherwise.");
-    main();
+    android_main();
 }
