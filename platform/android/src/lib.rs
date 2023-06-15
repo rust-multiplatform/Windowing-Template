5b09c8d25b2151841298f07bbe79b0e3a2524684
Lukas Weber <dev@sakul-flee.de>
2023-06-15 06:00:14 +0200

    Implemented new way of handling Android

--- platform/android/src/lib.rs
+++ platform/android/src/lib.rs
@@ -1,11 +1,43 @@
 #![allow(clippy::all)]
 
+use android_activity::{AndroidApp, InputStatus, MainEvent, PollEvent};
 use shared::entrypoint;
 
 #[cfg(test)]
 mod tests;
 
 #[no_mangle]
-fn android_main() {
-    entrypoint();
+fn android_main(app: AndroidApp) {
+    android_logger::init_once(android_logger::Config::default().with_min_level(log::Level::Info));
+
+    loop {
+        app.poll_events(
+            Some(std::time::Duration::from_millis(500)), /* timeout */
+            |event| {
+                match event {
+                    PollEvent::Wake => {
+                        log::info!("Early wake up");
+                    }
+                    PollEvent::Timeout => {
+                        entrypoint();
+                    }
+                    PollEvent::Main(main_event) => {
+                        log::info!("Main event: {:?}", main_event);
+                        match main_event {
+                            MainEvent::Destroy => {
+                                return;
+                            }
+                            _ => {}
+                        }
+                    }
+                    _ => {}
+                }
+
+                app.input_events(|event| {
+                    log::info!("Input Event: {event:?}");
+                    InputStatus::Unhandled
+                });
+            },
+        );
+    }
 }
