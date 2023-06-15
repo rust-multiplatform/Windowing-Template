d78cdbb2c61d26b55b2897a8d40c79ac0b543b78
Lukas Weber <dev@sakul-flee.de>
2023-06-15 03:33:32 +0200

    Disabled tests

--- platform/macos/src/tests/mod.rs
+++ platform/macos/src/tests/mod.rs
@@ -1,8 +1,8 @@
-use crate::main;
+// use crate::main;
 
-#[test]
-#[cfg(feature = "ui-tests")]
-fn check_main_exists() {
-    println!("This requires a Desktop Environment up and running! Tests will fail otherwise.");
-    main();
-}
+// #[test]
+// #[cfg(feature = "ui-tests")]
+// fn check_main_exists() {
+//     println!("This requires a Desktop Environment up and running! Tests will fail otherwise.");
+//     main();
+// }
